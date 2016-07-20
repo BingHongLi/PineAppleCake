dfaf3d6a0f0801915aaa106708cf97df420b7eb2
BingHongLi <b97607065@gmail.com>
2016-07-20 08:06:40 +0800

    modify  TestControllerTemplate

--- test-2.11/TestControllerTemplate.scala
+++ test-2.11/TestControllerTemplate.scala
@@ -11,12 +11,6 @@ import play.api.test._
 import play.api.test.Helpers._
 import scala.concurrent.Future
 
-// For Mockito object
-import org.scalatest.mock.MockitoSugar
-
-trait IntDataService{
-  def findData: List[Int]
-}
 
 class TestControllerTemplate extends PlaySpec with Results {
 
