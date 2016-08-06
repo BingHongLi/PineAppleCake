27f6d581c806a21cb80f5e41a53186235a028941
alisonyang <alisonyang333@gmail.com>
2016-08-06 20:55:50 +0800

    feature2 first commit

--- app-2.11/controllers/PineApple.scala
+++ app-2.11/controllers/PineApple.scala
@@ -1,42 +1,60 @@
 package controllers
 
 /**
-  * Created by BingHongLi on 2016/7/25.
+  * Created by alison on 2016/8/5.
   */
 
-import play.api.mvc.Action
-import play.api.mvc.Controller
+import play.api.mvc.{Action, BodyParsers, Controller}
 import play.api._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
-//JSON Example
+case class PineAppleJsonFormat(vote:Int,cake1Sum:Int,cake2Sum:Int,totalTickets:Int,leftTickets:Int,winner:Int)
 
-//request
-//{
-//"vote" : 0,
-//"cake 1 sum" : 6,
-//"cake 2 sum" : 3 ,
-//"total tickets": 11
-//"left tickets" : 2,
-//"winner": 0
-//}
-//
-//response
-//{
-//"vote" : 0,
-//"cake 1 sum" : 6,
-//"cake 2 sum" : 3 ,
-//"total tickets": 11
-//"left tickets" : 2,
-//"winner": 1
-//}
-
-class PineApple extends Controller with AccessLogging {
-
+class PineApple extends Controller with AccessLogging{
   val logger = Logger(this.getClass)
+
+  implicit val pineAppleFormat2 = Json.format[PineAppleJsonFormat]
 
   def pineApple1() = TODO
 
-  def pineApple2() = TODO
+  def pineApple2() = Action(BodyParsers.parse.json){ request =>{
+    logger.info("get request, we will parse the request body to an object")
+
+    request.body.validate[PineAppleJsonFormat] match {
+      case s :JsSuccess[PineAppleJsonFormat] =>{
+        val bodyJson = s.get
+        logger.info("caculate cake2Sum value")
+        if (bodyJson.leftTickets > 0){
+          val returnCake2Sum = bodyJson.vote match {
+            case 0 =>   bodyJson.cake2Sum
+            case 1 =>   bodyJson.cake2Sum
+            case 2 => bodyJson.cake2Sum + 1
+          }
+          val returnLeftTickets = bodyJson.leftTickets - 1
+          val pineAppleJson = PineAppleJsonFormat(
+            0,
+            bodyJson.cake1Sum,
+            returnCake2Sum,
+            bodyJson.totalTickets,
+            returnLeftTickets,
+            bodyJson.winner
+          )
+          logger.info("sent the resonpse with json")
+          Ok(Json.toJson(pineAppleJson))
+
+        }else{
+          logger.warn("invalid value input")
+          BadRequest("not valid")
+
+        }
+
+      }
+
+    }
+  }
+  }
+
 
   def pineAppleSum() = TODO
 
